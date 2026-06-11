4671e333b30c79271187227be4a21fb60a9285ed
ichaseon7 <ichaseon7@gmail.com>
2026-06-11 16:24:10 +0900

    update phone number to 010-9561-7332

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,8 +1,8 @@
 export const businessName = "대전세븐나이트 웨이터 딸기";
 export const area = "대전광역시 중구 유천동";
 export const detailAddress = "332-28";
-export const phoneDisplay = "010-9562-0035";
-export const phoneHref = "tel:01095620035";
+export const phoneDisplay = "010-9561-7332";
+export const phoneHref = "tel:01095617332";
 export const kakaoOpenChatHref = "https://open.kakao.com/o/strawberry7";
 export const siteUrl = "https://daejeon-seven-night.com";
 export const fullAddress = `${area} ${detailAddress}`;
